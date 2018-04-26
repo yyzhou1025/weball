ab351623e28d4490f705b2839efa0c9b44a06de1
yizhuanj <31459440+yizhuanj@users.noreply.github.com>
2018-04-25 21:30:21 -0400

    merge from yizhuanj

--- app/src/main/java/com/example/yuanyuanzhou/weball/home_page.java
+++ app/src/main/java/com/example/yuanyuanzhou/weball/home_page.java
@@ -12,8 +12,12 @@ public class home_page extends Activity {
         setContentView(R.layout.activity_home_page);
     }
 
-    public void logoutButtonButton (View view){
+    public void logoutButton (View view){
         Intent intent = new Intent (this, login_page.class);
+        startActivity(intent);
+    }
+    public void buttonCreate (View view){
+        Intent intent = new Intent (this, Create1Activity.class);
         startActivity(intent);
     }
 }
